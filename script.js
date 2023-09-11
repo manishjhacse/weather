7a6157a21f0ed319449bc5f8583e58aa58827121
Manish Kumar Jha <123859854+manishjhacse@users.noreply.github.com>
2023-09-11 16:13:07 +0530

    change image according to weather

--- script.js
+++ script.js
@@ -6,32 +6,37 @@ async function checkWeather(city)
     const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
     var data=await response.json();
     console.log(data);
-    document.querySelector("#display-city").innerHTML=data.name;
+    if(data.name==undefined){
+        document.querySelector("#display-city").innerHTML="No data available for this city";
+    }
+    else{
+        document.querySelector("#display-city").innerHTML=data.name;
+    }
     document.querySelector("#temp").innerHTML=Math.round(data.main.temp);
     document.querySelector("#humidity").innerHTML=data.main.humidity;
     document.querySelector("#windspeed").innerHTML=data.wind.speed;
     const weatherCondition =data.weather[0].main;
     if(weatherCondition=="Clouds")
     {
-        displayImage.src="/images/clouds.png"
+        displayImage.src="images/clouds.png"
     }
     else if(weatherCondition=="Clear"){
-        displayImage.src="/images/clear.png"
+        displayImage.src="images/clear.png"
     }
     else if(weatherCondition=="Rain"){
-        displayImage.src="/images/rain.png"
+        displayImage.src="images/rain.png"
     }
     else if(weatherCondition=="Drizzle"){
-        displayImage.src="/images/drizzle.png"
+        displayImage.src="images/drizzle.png"
     }
     else if(weatherCondition=="Mist"){
-        displayImage.src="/images/mist.png"
+        displayImage.src="images/mist.png"
     }
     else if(weatherCondition=="Snow"){
-        displayImage.src="/images/snow.png"
+        displayImage.src="images/snow.png"
     }
     else{
-        displayImage.src="/images/clear.png"
+        displayImage.src="images/clear.png"
     }
 
     
